fa340e348b8544bd7aee26e18ada2ee3c165ddbc
Murali <muralikish_1@hotmail.com>
2019-02-14 16:13:39 +0530

    commit

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -5,9 +5,9 @@ var {generateMessage, generateLocationMessage} = require('./message');
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var from = 'Jen';
-    var text = 'Some message';
+    var text = 'some message';
     var message = generateMessage(from, text);
-
+   
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
   });
